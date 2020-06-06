e2fb9b6e26c255706d4821d56bd62ef60cbab62b
blue96 <pascalfelim@yahoo.com>
2020-06-06 13:20:18 +0700

    update

--- sesi 9 4/index.js
+++ sesi 9 4/index.js
@@ -4,6 +4,8 @@ let newAngka = angka.map(element=>{
     return element * 5
 })
 
+document.getElementById('hasil').innerHTML = `Array Lama = ${angka} <br> Array Baru = ${newAngka}` ;
+
 console.log('angka lama')
 console.log(angka)
 console.log('angka baru')
