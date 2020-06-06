4f1a11d9488b47289f80e3a52b171c313dbcc263
blue96 <pascalfelim@yahoo.com>
2020-06-06 10:28:34 +0700

    tutorial 4

---
+++ sesi 9 4/index.js
@@ -0,0 +1,10 @@
+let angka = [3,5,7,9,11]
+
+let newAngka = angka.map(element=>{
+    return element * 5
+})
+
+console.log('angka lama')
+console.log(angka)
+console.log('angka baru')
+console.log(newAngka)
