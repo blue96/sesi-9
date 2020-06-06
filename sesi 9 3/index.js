fe26e67e43bb3071c86e197b612e54ca7a5472fe
blue96 <pascalfelim@yahoo.com>
2020-06-06 10:18:54 +0700

    revisi

--- sesi 9 3/index.js
+++ sesi 9 3/index.js
@@ -1,6 +1,6 @@
 let nama = ['Dhanang Prasetya','Ringgas W Sarumpaet','Aldi Kesuma','M. Abdillah','Mawan Irwansyah','Alhambra AR','Ahmad Irfansyah','Agus Prabowo','Pascal Felim','M. Rizki Novriandri','Ariel Busy Andalas','Didi Alfansyah']
 
-document.getElementById('hasil').innerHTML = nama.join();
+document.getElementById('hasil').innerHTML = nama.join(' ; ');
 console.log('data array adalah')
 console.log(nama)
-console.log('data setelah di join ' + nama.join());
+console.log('data setelah di join ' + nama.join(' ; '));
